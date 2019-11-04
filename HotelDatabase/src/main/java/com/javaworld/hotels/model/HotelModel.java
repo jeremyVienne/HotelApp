532690ff9830ac9b1c399d2d0d6287379b84fd96
Vienne Jeremy <vienne@b10p27.fil.univ-lille1.fr>
2019-11-04 09:27:59 +0100

    test

--- HotelDatabase/src/main/java/com/javaworld/hotels/model/HotelModel.java
+++ HotelDatabase/src/main/java/com/javaworld/hotels/model/HotelModel.java
@@ -31,6 +31,7 @@ public class HotelModel {
 	{
 		"Paris",
 		"London",
+		"Grand place",
 	};
 
 	/**
@@ -42,7 +43,7 @@ public class HotelModel {
 		new Hotel("Hotel Vendome","Place Vendome","Paris",5),
 		new Hotel("Hotel Hilton","Trafalgar Square","London",4),
 		new Hotel("Hotel Ibis","The City","London",3),
-		new Hotel("Hotel Cigogne", "Grand place", 2)
+		new Hotel("Hotel Cigogne", "Grand place", 2),
 	};
 
 	/**
